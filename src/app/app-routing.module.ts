9f214b43ce8585bc9c8ab83edf735a16c4a6d640
Jacek Gizinski <jacektrg+github@gmail.com>
2022-11-16 18:29:17 +0100

    Adding routes components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CatalogComponent } from './components/catalog/catalog.component';
+import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: CatalogComponent },
+  { path: ':id', component: MovieDetailsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
