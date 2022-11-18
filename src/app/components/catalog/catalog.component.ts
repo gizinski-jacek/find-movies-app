ad888a199922d91e5d2fcb73c38547b002e574a1
Jacek Gizinski <jacektrg+github@gmail.com>
2022-11-18 18:58:36 +0100

    Importing ActivatedRoute.
    Subscribing to changes to route params to make approriate api call.
    Adding array state to keep track of subscriptions and to
    properly unsubscribe from them.

--- src/app/components/catalog/catalog.component.ts
+++ src/app/components/catalog/catalog.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { HttpService } from 'src/app/services/http.service';
 import { SearchService } from 'src/app/services/search.service';
 import { Movie } from 'src/types/types';
@@ -10,23 +12,49 @@ import { Movie } from 'src/types/types';
 })
 export class CatalogComponent implements OnInit {
   collection: Movie[] = [];
-  searchCollection: Movie[] = [];
+  searchCollection: Movie[] | null = null;
+  subscriptions: Subscription[] = [];
 
   constructor(
     private httpService: HttpService,
-    private searchService: SearchService
+    private searchService: SearchService,
+    private route: ActivatedRoute
   ) {}
 
-  ngOnInit() {
-    this.httpService.getRandomMovies().subscribe({
-      next: (res) => {
-        const random = res.results.slice(0, 8);
-        this.collection = random;
-      },
-      error: (err) => console.log(err),
+  ngOnInit(): void {
+    this.route.params.subscribe(({ type, genre }) => {
+      if (!genre) {
+        const subscription = this.httpService
+          .getRandomCollection(type)
+          .subscribe({
+            next: (res) => {
+              const random = res.results.slice(0, 8);
+              console.log(random);
+              this.collection = random;
+            },
+            error: (err) => console.log(err),
+          });
+        this.subscriptions.push(subscription);
+      } else {
+        const subscription = this.httpService
+          .getGenreCollection(type, genre)
+          .subscribe({
+            next: (res) => {
+              const random = res.results.slice(0, 8);
+              this.collection = random;
+            },
+            error: (err) => console.log(err),
+          });
+        this.subscriptions.push(subscription);
+      }
     });
-    this.searchService.$searchData.subscribe(
+    const searchSubscription = this.searchService.$searchData.subscribe(
       (data) => (this.searchCollection = data)
     );
+    this.subscriptions.push(searchSubscription);
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((s) => s.unsubscribe());
   }
 }
