6935f2ee4691e41cf5ba6fb2dd37cc4051368c2f
Jacek Gizinski <jacektrg+github@gmail.com>
2022-11-18 18:40:12 +0100

    Cleaning up.
    Adding Genre interface

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,21 +1,21 @@
 export interface Movie {
+  poster_path: string;
   adult: boolean;
-  backdrop_path: string | null;
+  overview: string;
+  release_date: string;
   genre_ids: number[];
   id: number;
-  original_language: string;
   original_title: string;
-  overview: string;
-  popularity: number;
-  poster_path: string;
-  release_date: string;
+  original_language: string;
   title: string;
+  backdrop_path: string | null;
+  popularity: number;
+  vote_count: number;
   video: boolean;
   vote_average: number;
-  vote_count: number;
 }
 
-export interface ApiResponse {
-  [key: string]: any;
-  results: Movie[];
+export interface Genre {
+  id: number;
+  name: string;
 }
