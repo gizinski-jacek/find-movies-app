43c10fdff6b106af4a767b3ce4eea0ab9bb41a38
Jacek Gizinski <jacektrg+github@gmail.com>
2022-11-18 19:01:37 +0100

    Fetching movie genre list OnInit
    Using location.path() for now to get :type param since header is
    not inside routes.
    Adding array state to keep track of subscriptions and to
    properly unsubscribe from them.
    Updating types.

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,8 +1,9 @@
 import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { HttpService } from 'src/app/services/http.service';
 import { SearchService } from 'src/app/services/search.service';
-import { Movie } from 'src/types/types';
+import { Genre, Movie } from 'src/types/types';
 
 @Component({
   selector: 'app-header',
@@ -10,13 +11,14 @@ import { Movie } from 'src/types/types';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-  title: string = 'Find Movies';
   attribution: string = 'Powered By <TMDB Logo Here>';
   timeout: any = null;
   searchValue: string = '';
-  searchCollection: Movie[] = [];
+  searchCollection: Movie[] | null = null;
   inputFocus: boolean = false;
   renderResults: boolean = false;
+  genres: Genre[] = [];
+  subscriptions: Subscription[] = [];
 
   constructor(
     private http: HttpService,
@@ -24,28 +26,47 @@ export class HeaderComponent implements OnInit {
     private searchService: SearchService
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    const movieGenresSubscription = this.http
+      .getMovieGenres()
+      .subscribe({ next: (res) => (this.genres = res.genres) });
+    this.subscriptions.push(movieGenresSubscription);
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((s) => s.unsubscribe());
+  }
 
   onChange() {
-    if (!this.searchValue || this.searchValue.length < 3) return;
+    if (!this.searchValue) {
+      this.searchService.changeSearchData(null);
+      return;
+    }
+    if (this.searchValue.length < 3) {
+      return;
+    }
     clearTimeout(this.timeout);
     this.timeout = setTimeout(() => {
-      this.http.searchMovie(this.searchValue).subscribe({
-        next: (res) => {
-          if (this.location.path() === '') {
-            this.searchService.changeSearchData(res.results);
-          } else {
-            this.searchCollection = res.results;
-          }
-        },
-        error: (err) => console.log(err),
-      });
+      const path = this.location.path().split('?')[0].replace('/', '');
+      const subscription = this.http
+        .searchForCollection(path, this.searchValue)
+        .subscribe({
+          next: (res) => {
+            if (path === 'movie') {
+              this.searchService.changeSearchData(res.results);
+            } else {
+              this.searchCollection = res.results;
+            }
+          },
+          error: (err) => console.log(err),
+        });
+      this.subscriptions.push(subscription);
     }, 500);
   }
 
   resetInput() {
     this.searchValue = '';
-    this.searchService.changeSearchData([]);
+    this.searchService.changeSearchData(null);
   }
 
   onFocus() {
