bf5c98f700721cdc173740c8ec649af5482c3ee3
Jacek Gizinski <jacektrg+github@gmail.com>
2022-11-20 23:55:10 +0100

    Adding APIResponse and MovieDetails types.

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,5 +1,5 @@
 export interface Movie {
-  poster_path: string;
+  poster_path: string | null;
   adult: boolean;
   overview: string;
   release_date: string;
@@ -15,7 +15,42 @@ export interface Movie {
   vote_average: number;
 }
 
+export interface MovieDetails extends Omit<Movie, 'genre_ids' | 'overview'> {
+  belongs_to_collection: null | object;
+  budget: number;
+  genres: {
+    id: number;
+    name: string;
+  }[];
+  homepage: string | null;
+  imdb_id: string | null;
+  overview: string | null;
+  production_companies: {
+    id: number;
+    logo_path: string | null;
+    name: string;
+    origin_country: string;
+  }[];
+  production_countries: {
+    iso_3166_1: string;
+    name: string;
+  }[];
+  release_date: string;
+  revenue: number;
+  runtime: number;
+  spoken_languages: {
+    iso_639_1: string;
+    name: string;
+  }[];
+  status: string;
+  tagline: string | null;
+}
+
 export interface Genre {
   id: number;
   name: string;
+}
+
+export interface APIResponse<T> {
+  [key: string]: T[];
 }
