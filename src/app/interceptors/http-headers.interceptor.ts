5859852ea4a84f94423eb9c311adfd4ece943204
Jacek Gizinski <jacektrg+github@gmail.com>
2022-11-20 23:58:45 +0100

    Adding http headers injector.

---
+++ src/app/interceptors/http-headers.interceptor.ts
@@ -0,0 +1,21 @@
+import { HttpEvent, HttpHandler, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable()
+export class HttpHeadersInterceptor {
+  constructor() {}
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    req = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${environment.tmdbAPIKey}`,
+      },
+    });
+    return next.handle(req);
+  }
+}
