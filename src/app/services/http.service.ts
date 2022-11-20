8f7a64b6af72cbddb0caa6bff0e31ec4d67aae5e
Jacek Gizinski <jacektrg+github@gmail.com>
2022-11-20 23:59:10 +0100

    Updating types.
    Cleaning up after adding http injectors.

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -2,57 +2,51 @@ import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { Movie, Genre } from 'src/types/types';
+import { Movie, Genre, APIResponse, MovieDetails } from 'src/types/types';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HttpService {
-  private url = environment.tmdbEndpoint;
-  private key = environment.tmdbAPIKey;
-  private headers: HttpHeaders = new HttpHeaders({
-    Authorization: `Bearer ${this.key}`,
-  });
+  private endpoint = environment.tmdbEndpoint;
 
   constructor(private http: HttpClient) {}
 
-  getRandomCollection(type: string): Observable<{ results: Movie[] }> {
-    return this.http.get<{ results: Movie[] }>(`${this.url}/discover/${type}`, {
-      headers: this.headers,
-    });
+  getRandomCollection(type: string): Observable<APIResponse<Movie>> {
+    return this.http.get<APIResponse<Movie>>(
+      `${this.endpoint}/discover/${type}`
+    );
   }
 
   searchForCollection(
     type: string,
     query: string
-  ): Observable<{ results: Movie[] }> {
+  ): Observable<APIResponse<Movie>> {
     const params = new HttpParams({ fromString: `query=${query}` });
-    return this.http.get<{ results: Movie[] }>(`${this.url}/search/${type}`, {
-      headers: this.headers,
-      params: params,
-    });
+    return this.http.get<APIResponse<Movie>>(
+      `${this.endpoint}/search/${type}`,
+      { params: params }
+    );
   }
 
-  getMovieDetails(id: string): Observable<Movie> {
-    return this.http.get<Movie>(`${this.url}/movie/${id}`, {
-      headers: this.headers,
-    });
+  getMovieDetails(id: string): Observable<MovieDetails> {
+    return this.http.get<MovieDetails>(`${this.endpoint}/movie/${id}`, {});
   }
 
-  getMovieGenres(): Observable<{ genres: Genre[] }> {
-    return this.http.get<{ genres: Genre[] }>(`${this.url}/genre/movie/list`, {
-      headers: this.headers,
-    });
+  getMovieGenres(): Observable<APIResponse<Genre>> {
+    return this.http.get<APIResponse<Genre>>(
+      `${this.endpoint}/genre/movie/list`
+    );
   }
 
   getGenreCollection(
     type: string,
     genre: string
-  ): Observable<{ results: Movie[] }> {
+  ): Observable<APIResponse<Movie>> {
     const params = new HttpParams({ fromString: `with_genres=${genre}` });
-    return this.http.get<{ results: Movie[] }>(`${this.url}/discover/${type}`, {
-      headers: this.headers,
-      params: params,
-    });
+    return this.http.get<APIResponse<Movie>>(
+      `${this.endpoint}/discover/${type}`,
+      { params: params }
+    );
   }
 }
