57c8521c8bb5ee90e76841d11c89d54291f9316c
Jacek Gizinski <jacektrg+github@gmail.com>
2022-11-21 18:17:58 +0100

    Renaming component.
    Updating httpService functions.

--- src/app/components/movie-catalog/movie-catalog.component.spec.ts
+++ src/app/components/movie-catalog/movie-catalog.component.spec.ts
@@ -3,21 +3,21 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
-import { CatalogComponent } from './catalog.component';
+import { MovieCatalogComponent } from './movie-catalog.component';
 
-describe('CatalogComponent', () => {
-  let component: CatalogComponent;
-  let fixture: ComponentFixture<CatalogComponent>;
+describe('MovieCatalogComponent', () => {
+  let component: MovieCatalogComponent;
+  let fixture: ComponentFixture<MovieCatalogComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CatalogComponent ]
+      declarations: [ MovieCatalogComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(CatalogComponent);
+    fixture = TestBed.createComponent(MovieCatalogComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
---
+++ src/app/components/movie-catalog/movie-catalog.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { HttpService } from 'src/app/services/http.service';
+import { SearchService } from 'src/app/services/search.service';
+import { Movie } from 'src/types/types';
+
+@Component({
+  selector: 'app-movie-catalog',
+  templateUrl: './movie-catalog.component.html',
+  styleUrls: ['./movie-catalog.component.scss'],
+})
+export class MovieCatalogComponent implements OnInit {
+  collection: Movie[] = [];
+  searchCollection: Movie[] | null = null;
+  subs: Subscription[] = [];
+
+  constructor(
+    private httpService: HttpService,
+    private searchService: SearchService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe(({ genre }) => {
+      const sub = this.httpService.getRandomMovies(genre).subscribe((res) => {
+        const random = res['results'].slice(0, 8);
+        this.collection = random;
+      });
+      this.subs.push(sub);
+    });
+    const searchSub = this.searchService.$movieSearchData.subscribe(
+      (data) => (this.searchCollection = data)
+    );
+    this.subs.push(searchSub);
+  }
+
+  ngOnDestroy(): void {
+    this.subs.forEach((s) => s.unsubscribe());
+  }
+}
