f9155b04194db1f0430c29164a831f5876833f39
Jacek Gizinski <jacektrg+github@gmail.com>
2022-11-21 19:03:36 +0100

    Importing new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,21 +9,27 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FooterComponent } from './components/footer/footer.component';
 import { MovieItemComponent } from './components/movie-item/movie-item.component';
 import { FormsModule } from '@angular/forms';
-import { CatalogComponent } from './components/catalog/catalog.component';
 import { CommonModule } from '@angular/common';
 import { HttpErrorsInterceptor } from './interceptors/http-errors.interceptor';
 import { HttpHeadersInterceptor } from './interceptors/http-headers.interceptor';
 import { RouterParamService } from './services/router-param.service';
 import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
+import { MovieCatalogComponent } from './components/movie-catalog/movie-catalog.component';
+import { TvShowCatalogComponent } from './components/tv-show-catalog/tv-show-catalog.component';
+import { TvShowItemComponent } from './components/tv-show-item/tv-show-item.component';
+import { TvShowDetailsComponent } from './components/tv-show-details/tv-show-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    CatalogComponent,
+    MovieCatalogComponent,
     MovieItemComponent,
     MovieDetailsComponent,
+    TvShowCatalogComponent,
+    TvShowItemComponent,
+    TvShowDetailsComponent,
   ],
   imports: [
     BrowserModule,
