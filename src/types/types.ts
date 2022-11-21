fbf742d1f2105855f4b2552672f56e5aed300f60
Jacek Gizinski <jacektrg+github@gmail.com>
2022-11-21 18:18:36 +0100

    Adding TvShow and TvShowDetails interfaces

--- src/types/types.ts
+++ src/types/types.ts
@@ -46,6 +46,86 @@ export interface MovieDetails extends Omit<Movie, 'genre_ids' | 'overview'> {
   tagline: string | null;
 }
 
+export interface TvShow {
+  poster_path: string | null;
+  popularity: number;
+  id: number;
+  backdrop_path: string | null;
+  vote_average: number;
+  overview: string;
+  first_air_date: string;
+  origin_country: string[];
+  genre_ids: number[];
+  original_language: string;
+  vote_count: number;
+  name: string;
+  original_name: string;
+}
+
+export interface TvShowDetails extends Omit<TvShow, 'genre_ids'> {
+  created_by: {
+    id: number;
+    credit_id: string;
+    name: string;
+    gender: number;
+    profile_path: string | null;
+  }[];
+  episode_run_time: number[];
+  genres: { id: number; name: string }[];
+  homepage: string;
+  in_production: boolean;
+  languages: string[];
+  last_air_date: string;
+  last_episode_to_air: {
+    air_date: string;
+    episode_number: number;
+    id: number;
+    name: string;
+    overview: string;
+    production_code: string;
+    season_number: number;
+    still_path: string | null;
+    vote_average: number;
+    vote_count: number;
+  };
+  next_episode_to_air: null;
+  networks: {
+    name: string;
+    id: number;
+    logo_path: string | null;
+    origin_country: string;
+  }[];
+  number_of_episodes: number;
+  number_of_seasons: number;
+  production_companies: {
+    id: number;
+    logo_path: null | string;
+    name: string;
+    origin_country: string;
+  }[];
+  production_countries: {
+    iso_3166_1: string;
+    name: string;
+  }[];
+  seasons: {
+    air_date: string;
+    episode_count: number;
+    id: number;
+    name: string;
+    overview: string;
+    poster_path: string;
+    season_number: number;
+  }[];
+  spoken_languages: {
+    english_name: string;
+    iso_639_1: string;
+    name: string;
+  }[];
+  status: string;
+  tagline: string | null;
+  type: string;
+}
+
 export interface Genre {
   id: number;
   name: string;
